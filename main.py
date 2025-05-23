229c3f283d2c9d6c263592ef385e8acaa2530420
doanleminh <doanleminhsptk45@gmail.com>
2025-05-23 09:26:24 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
+
 from flask import Flask
+from bs4 import BeautifulSoup
+import requests
 import threading
 import time
-import os
-import requests
 import datetime
-from bs4 import BeautifulSoup
+import os
+import psutil
 
-URL = "https://doithe365.com/doithecao"
+# Telegram token & chat_id
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID") or "5768955670"
+
+URL = "https://doithe365.com/"
 app = Flask(__name__)
 
-
 def send_telegram(rate):
-    token = os.getenv("TELEGRAM_TOKEN")
-    chat_id = "5768955670"
-    if not token or not chat_id:
-        print("Thiếu TELEGRAM_TOKEN hoặc TELEGRAM_CHAT_ID")
+    if not TELEGRAM_TOKEN:
+        print("Thiếu TELEGRAM_TOKEN", flush=True)
         return
-
-    message = f"Chiết khấu Vinaphone 500K hiện tại là {rate}%\nhttps://doithe365.com/doithecao"
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
-
     try:
-        response = requests.post(url, data=data)
-        if response.status_code == 200:
-            print("Đã gửi Telegram.")
-        else:
-            print("Gửi Telegram thất bại:", response.text)
+        message = f"Chiết khấu Vinaphone 500K hiện tại là {rate}%."
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        data = {
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": message,
+        }
+        requests.post(url, data=data, timeout=10)
     except Exception as e:
-        print("Lỗi gửi Telegram:", e)
-
+        print(f"Lỗi khi gửi Telegram: {e}", flush=True)
 
 def check_discount():
     print(f"Đang kiểm tra chiết khấu...", flush=True)
     try:
-        res = requests.get(URL)
+        res = requests.get(URL, timeout=10)
         soup = BeautifulSoup(res.text, "html.parser")
 
         tab = soup.find("div", id="VINAPHONE")
@@ -69,21 +68,21 @@ def check_discount():
     except Exception as e:
         print(f"Lỗi khi xử lý: {e}", flush=True)
     finally:
+        mem = psutil.Process(os.getpid()).memory_info().rss / 1024**2
+        print(f"Memory usage: {mem:.2f} MB", flush=True)
         print("Hoàn tất kiểm tra\n", flush=True)
-
+        del res, soup  # giải phóng bộ nhớ
 
 def run_loop():
     while True:
         check_discount()
         time.sleep(30)
 
-
 @app.route("/")
-def home():
+def index():
     return "Vinaphone monitor is running!"
 
-
-threading.Thread(target=run_loop).start()
-
 if __name__ == "__main__":
+    thread = threading.Thread(target=run_loop, daemon=True)
+    thread.start()
     app.run(host="0.0.0.0", port=8080)
